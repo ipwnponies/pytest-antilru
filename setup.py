e2bc3967e5e05271457e784e249a96f80bd92b9b
John Nguyen <ipwnponies@gmail.com>
2019-02-19 01:50:36 -0800

    Fix setup
    Becuase I typo a lot

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '''Caching with functools.lru_cache is great for performance. It works so well that it'll even
+LONG_DESCRIPTION = '''Caching with functools.lru_cache is great for performance. It works so well that it'll even
 speed up your unit test runs. All you need to sacrifice in return is test isolation and your sanity.
 
 Imagine, you mock some things out and a function caches those results. On your next test run, it doesn't matter what you
@@ -10,17 +10,17 @@ mock, the results are already cached. Now trying running those two test out-of-o
 '''
 
 setup(
-    name='myproject',
+    name='pytest-antilru',
     version='0.1.0',
     description='Bust functools.lru_cache when running pytest to avoid test pollution',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(exclude=('tests/*')),
     install_requires=[
         'six',
     ],
-    entry_points={"pytest11": ["name_of_plugin = pytest_antilru"]},
+    entry_points={'pytest11': ['name_of_plugin = pytest_antilru.main']},
     classifiers=[
-        "Framework :: Pytest",
+        'Framework :: Pytest',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
