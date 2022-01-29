2c1ac3838c941e4d3fba71da52f7cb0de26e56ab
John Nguyen <ipwnponies@gmail.com>
2022-01-29 01:09:37 -0800

    Add test cases for lru_cache as plain decorator
    This is decorating lru_cache without invocation.

--- tests/main_test.py
+++ tests/main_test.py
@@ -20,17 +20,41 @@ def expensive_network_call():
     return 1
 
 
-@lru_cache()
-def cache_me():
+@lru_cache(128, False)
+def cache_me_lru_cache_explicit_param():
     return expensive_network_call()
 
 
-def test_a_run_first():
-    assert cache_me() == 1
+@lru_cache
+def cache_me_default_param():
+    return expensive_network_call()
 
 
-def test_b_run_second():
-    # We want to mock the network call for this test case
-    with mock.patch.object(sys.modules[__name__], 'expensive_network_call', return_value=2) as mock_network_call:
-        assert cache_me() == 2
-        assert mock_network_call.called
+class TestLruCacheExplicitParam:
+    @staticmethod
+    def test_a_run_first():
+        '''Run this test first, to pollute the test environment.'''
+        assert cache_me_lru_cache_explicit_param() == 1
+
+    @staticmethod
+    def test_b_run_second():
+        '''Run second, after env is dirtied.'''
+        # We want to mock the network call for this test case
+        with mock.patch.object(sys.modules[__name__], 'expensive_network_call', return_value=2) as mock_network_call:
+            assert cache_me_lru_cache_explicit_param() == 2
+            assert mock_network_call.called
+
+
+class TestLruCacheDefaultParam:
+    @staticmethod
+    def test_a_run_first():
+        '''Run this test first, to pollute the test environment.'''
+        assert cache_me_default_param() == 1
+
+    @staticmethod
+    def test_b_run_second():
+        '''Run second, after env is dirtied.'''
+        # We want to mock the network call for this test case
+        with mock.patch.object(sys.modules[__name__], 'expensive_network_call', return_value=2) as mock_network_call:
+            assert cache_me_default_param() == 2
+            assert mock_network_call.called
