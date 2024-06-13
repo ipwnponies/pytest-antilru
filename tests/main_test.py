d6f191c951cee78064f1382e1e3a1ab37ba32d17
John Nguyen <ipwnponies@gmail.com>
2024-06-13 00:09:49 -0700

    Test lru_cache with and without parameters
    Python decorators can be tricky so test that we have wrapped the
    decorator correctly.

--- tests/main_test.py
+++ tests/main_test.py
@@ -23,14 +23,19 @@ def expensive_network_call():
     return 1
 
 
-@lru_cache(128)
-def cache_me_lru_cache_explicit_param():
+@lru_cache
+def cache_me_lru_cache():
     return expensive_network_call()
+
+
+@lru_cache(1337, typed=True)
+def cache_me_lru_cache_explicit_param():  # pragma: no cover
+    return mock.sentinel.default_param
 
 
 def test_a_run_first():
     '''Run this test first, to pollute the test environment.'''
-    assert cache_me_lru_cache_explicit_param() == 1
+    assert cache_me_lru_cache() == 1
 
 
 def test_b_run_second():
@@ -39,16 +44,31 @@ def test_b_run_second():
     with mock.patch.object(
         sys.modules[__name__], 'expensive_network_call', return_value=2, autospec=True
     ) as mock_network_call:
-        assert cache_me_lru_cache_explicit_param() == 2
+        assert cache_me_lru_cache() == 2
         assert mock_network_call.called
 
 
-def test_lru_cache_unknown_kwargs():
-    '''Test that warning is emitted when new kwargs are added to lru_cache.
+class TestParameters:
+    def test_explicit_parameters(self):
+        '''Test the lru_cache parameters are wrapped correctly.'''
+        assert cache_me_lru_cache_explicit_param.cache_parameters() == {
+            'maxsize': 1337,
+            'typed': True,
+        }
 
-    Let's hope somene reports the warning and we can get to patching.
-    '''
-    with mock.patch.object(main.logging, 'warning', wraps=main.logging.warning) as spy:
-        lru_cache(new_feature=1)(expensive_network_call)
+    def test_default_parameters(self):
+        '''Test the default parameter is wrapped correctly.'''
+        assert cache_me_lru_cache.cache_parameters() == {
+            'maxsize': 128,
+            'typed': False,
+        }
 
-        assert spy.called
+    def test_lru_cache_unknown_kwargs(self):
+        '''Test that warning is emitted when new kwargs are added to lru_cache.
+
+        Let's hope somene reports the warning and we can get to patching.
+        '''
+        with mock.patch.object(main.logging, 'warning', wraps=main.logging.warning) as spy:
+            lru_cache(new_feature=1)(expensive_network_call)
+
+            assert spy.called
