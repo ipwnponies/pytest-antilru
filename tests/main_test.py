a217612d47bee70f2f6fe23e71644599186990a5
John Nguyen <ipwnponies@gmail.com>
2024-06-20 02:00:06 -0700

    Fix coverage with python 3.8
    Namely by pragma no cover code that uses lru_cache changes in 3.9

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,6 +2,8 @@ import sys
 from functools import lru_cache
 from unittest import mock
 
+import pytest
+
 from pytest_antilru import main
 
 
@@ -14,11 +16,6 @@ def expensive_network_call():
 @lru_cache
 def cache_me_lru_cache():
     return expensive_network_call()
-
-
-@lru_cache(1337, typed=True)
-def cache_me_lru_cache_explicit_param():  # pragma: no cover
-    return mock.sentinel.default_param
 
 
 def test_a_run_first():
@@ -36,27 +33,38 @@ def test_b_run_second():
         assert mock_network_call.called
 
 
+def test_lru_cache_unknown_kwargs():
+    '''Test that warning is emitted when new kwargs are added to lru_cache.
+
+    Let's hope somene reports the warning and we can get to patching.
+    '''
+    with mock.patch.object(main.logging, 'warning', wraps=main.logging.warning) as spy:
+        lru_cache(new_feature=1)(expensive_network_call)
+
+        assert spy.called
+
+
 class TestParameters:
-    def test_explicit_parameters(self):
+    @lru_cache(1337, typed=True)
+    def cache_me_lru_cache_explicit_param(self):
+        return mock.sentinel.default_param
+
+    def test(self):
+        assert self.cache_me_lru_cache_explicit_param() == mock.sentinel.default_param
+
+    @pytest.mark.skipif(sys.version_info < (3, 9), reason='cache_parameters added to Python 3.9')
+    def test_explicit_parameters(self):  # pragma: no cover <python39
         '''Test the lru_cache parameters are wrapped correctly.'''
-        assert cache_me_lru_cache_explicit_param.cache_parameters() == {
+        assert self.cache_me_lru_cache_explicit_param.cache_parameters() == {
             'maxsize': 1337,
             'typed': True,
         }
 
-    def test_default_parameters(self):
+    @pytest.mark.skipif(sys.version_info < (3, 9), reason='cache_parameters added to Python 3.9')
+    def test_default_parameters(self):  # pragma: no cover <python39
         '''Test the default parameter is wrapped correctly.'''
+
         assert cache_me_lru_cache.cache_parameters() == {
             'maxsize': 128,
             'typed': False,
         }
-
-    def test_lru_cache_unknown_kwargs(self):
-        '''Test that warning is emitted when new kwargs are added to lru_cache.
-
-        Let's hope somene reports the warning and we can get to patching.
-        '''
-        with mock.patch.object(main.logging, 'warning', wraps=main.logging.warning) as spy:
-            lru_cache(new_feature=1)(expensive_network_call)
-
-            assert spy.called
